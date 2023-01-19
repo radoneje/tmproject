92083b44f1f69a34cda47f60fc32b65dd2207cb3
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-19 22:22:09 +0300

    s

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -11,6 +11,9 @@ router.get('/', adminAuth, function(req, res, next) {
 });
 router.get('/general', adminAuth, async (req, res, next)=> {
   let r = await req.db.collection('general').find();
+  if(r._eventsCount==0){
+    r = await req.db.collection('general').insertMany([{id:"projectCount",value:0}])
+  }
   res.json(r)
 });
 
