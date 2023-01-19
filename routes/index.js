3845459b40de84ac88b2970d43c844327c11611d
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-19 19:12:23 +0300

    s

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let r=await  req.mongo.listDatabases(client);
+  let r=await  req.mongo.db().admin().listDatabases();
   console.log(r)
   res.render('index', { title: 'Express' });
 });
