dc5d1133e9e91382107eaa91e4fc1edc3c5ee76a
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-19 22:34:40 +0300

    s

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -10,7 +10,7 @@ router.get('/', adminAuth, function(req, res, next) {
   res.render("admin", {moment})
 });
 router.get('/general', adminAuth, async (req, res, next)=> {
-  let ret =  req.db.collection('general').findOne({id:"projectCount"})
+  let ret =  await req.db.collection('general').findOne({id:"projectCount"})
   res.json(ret)
 });
 
