f18d9449803d1b920046ae5572b4a76ba58e7163
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-19 22:18:09 +0300

    s

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', (req, res, next)=>{
   req.mongo=mongo;
-  req.db=app.db;
+  req.db=mongo.db('tmproject')
   next();
 });
 app.use('/', indexRouter);
