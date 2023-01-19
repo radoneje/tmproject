2405ed532c36ee0072436a5d147c4df72a616956
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-19 23:06:27 +0300

    s

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const moment=require("moment")
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   let r=await  req.mongo.db().admin().listDatabases();
+  console.log(moment().format("YYYY"))
   res.render('index', { title: 'Express', moment:moment });
 });
 
