e0c569839128fa9785026b66b4c3cfa70c07e3d9
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-19 22:18:49 +0300

    s

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -10,7 +10,7 @@ router.get('/', adminAuth, function(req, res, next) {
   res.render("admin", {moment})
 });
 router.get('/general', adminAuth, async (req, res, next)=> {
-  let r = await app.db.collection('menu')
+  let r = await req.db.collection('menu')
   res.json(r)
 });
 
