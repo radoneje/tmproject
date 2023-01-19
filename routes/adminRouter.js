0e8192eb619298262401e4feeb9aa3f49694222e
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-19 20:50:21 +0300

    s

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const moment=require("moment")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render("admin")
+  res.render("admin", {moment})
 });
 
 module.exports = router;
