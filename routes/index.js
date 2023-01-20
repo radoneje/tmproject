3206f42268306cdb311b602363514fd344928291
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-20 13:14:05 +0300

    s

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ router.get('/file/:filename', async function(req, res, next) {
 });
 router.get('/video/:_id', async (req, res, next)=> {
  // let video =  await req.db.collection('videos').findOne({_id:ObjectId(req.params._id)})
-  res.render("video", {video})
+  res.render("video", {video:{}})
 });
 
 module.exports = router;
