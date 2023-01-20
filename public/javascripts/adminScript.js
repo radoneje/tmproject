fa61f729f874b5f90f9bd27f63f871d23d31c671
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-21 00:29:30 +0300

    s

--- public/javascripts/adminScript.js
+++ public/javascripts/adminScript.js
@@ -1,146 +1,157 @@
-let app=new Vue({
-    el:"#app",
-    data:{
-        section:0,
-        general:{mainImgUrl:null},
-        videos:[],
-        sections:[],
-        services:{},
-        commands:[],
-        geografy:{}
+let app = new Vue({
+    el: "#app",
+    data: {
+        section: 0,
+        general: {mainImgUrl: null},
+        videos: [],
+        sections: [],
+        services: {},
+        commands: [],
+        geografy: {}
     },
-    methods:{
-        toggleGeografy:async function(section, item, prm){
-            item[prm]=!item[prm];
+    methods: {
+        addGeoItem: async function (section, item) {
+            item.items.unshift({
+                id:moment().unix(),
+                title:"",
+                isActive:false,
+                isDeleted:false
+            })
+            this.$forceUpdate();
+            await this.saveService(section, item)
+        },
+        toggleGeografy: async function (section, item, prm) {
+            item[prm] = !item[prm];
 
             await this.saveService(section, item)
-            if(prm=="isDeleted")
-                this.geografy[section] = this.geografy[section].filter(f=>!f.isDeleted)
+            if (prm == "isDeleted")
+                this.geografy[section] = this.geografy[section].filter(f => !f.isDeleted)
             this.$forceUpdate();
         },
-        toggleService:async function(section, item, prm){
-            item[prm]=!item[prm];
+        toggleService: async function (section, item, prm) {
+            item[prm] = !item[prm];
             await this.saveService(section, item)
-            if(prm=="isDeleted")
-                this.services[section] = this.services[section].filter(f=>!f.isDeleted)
+            if (prm == "isDeleted")
+                this.services[section] = this.services[section].filter(f => !f.isDeleted)
             this.$forceUpdate();
         },
-        saveService:async function(section, item){
+        saveService: async function (section, item) {
             await axios.post("/admin/saveService", {section, item});
         },
-        addGeografy:async function(section){
-            let r=await axios.post("/admin/addService", {section});
-            if(!this.geografy[section])
-                this.geografy[section]=[];
+        addGeografy: async function (section) {
+            let r = await axios.post("/admin/addService", {section});
+            if (!this.geografy[section])
+                this.geografy[section] = [];
             this.geografy[section].unshift(r);
             this.$forceUpdate();
         },
-        addService:async function(section){
+        addService: async function (section) {
 
-            let r=await axios.post("/admin/addService", {section});
-            if(!this.services[section])
-                this.services[section]=[];
+            let r = await axios.post("/admin/addService", {section});
+            if (!this.services[section])
+                this.services[section] = [];
             this.services[section].unshift(r);
             this.$forceUpdate();
         },
-        addImageVideo:async function(item){
-            await this.uploadFile(async (r)=>{
-                item.imageLink=r;
+        addImageVideo: async function (item) {
+            await this.uploadFile(async (r) => {
+                item.imageLink = r;
                 await this.saveVideo(item);
             })
         },
-        deleteVideo:async function(item){
-            if(!confirm("Вы уверены?"))
+        deleteVideo: async function (item) {
+            if (!confirm("Вы уверены?"))
                 return
-            item.isDeleted= new Date().toISOString();
+            item.isDeleted = new Date().toISOString();
             await this.saveVideo(item);
-            this.videos=this.videos.filter(v=>v.id!=item.id);
+            this.videos = this.videos.filter(v => v.id != item.id);
         },
-        changeVideoActive:async function(item){
-            item.isActive=!item.isActive;
+        changeVideoActive: async function (item) {
+            item.isActive = !item.isActive;
             await this.saveVideo(item)
         },
-        saveVideo:async function(item){
+        saveVideo: async function (item) {
             await axios.post("/admin/video", item)
         },
-        videoAdd:async function(){
-            let r=await axios.post("/admin/videoAdd")
+        videoAdd: async function () {
+            let r = await axios.post("/admin/videoAdd")
             this.videos.unshift(r.data)
         },
-        saveGeneral:async function(){
-            await axios.post("/admin/general",this.general )
+        saveGeneral: async function () {
+            await axios.post("/admin/general", this.general)
         },
-        uploadPreza:async function(){
+        uploadPreza: async function () {
 
-            await this.uploadFile(async (r)=>{
-                this.general.preza=r;
+            await this.uploadFile(async (r) => {
+                this.general.preza = r;
                 await this.saveGeneral()
             })
         },
-        uploadMainImage:async function(){
+        uploadMainImage: async function () {
 
-            await this.uploadFile(async (r)=>{
-                this.general.mainImgUrl=r;
+            await this.uploadFile(async (r) => {
+                this.general.mainImgUrl = r;
                 await this.saveGeneral()
             })
         },
-        uploadFile:async function(callBack){
-           let input=document.createElement("input")
+        uploadFile: async function (callBack) {
+            let input = document.createElement("input")
 
-            input.type="file";
-           input.onchange=async  (e)=>{
-               let fd = new FormData();
-               console.log("card", input.files[0]);
-               fd.append("card", input.files[0])
-               fd.append("ddd", "dd")
-               console.log("fd");
-               try {
-                   let r=await axios.post("/admin/uploadFile", fd, {headers: {
-                           "Content-Type": "multipart/form-data",
-                       }});
+            input.type = "file";
+            input.onchange = async (e) => {
+                let fd = new FormData();
+                console.log("card", input.files[0]);
+                fd.append("card", input.files[0])
+                fd.append("ddd", "dd")
+                console.log("fd");
+                try {
+                    let r = await axios.post("/admin/uploadFile", fd, {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                        }
+                    });
 
-                   callBack(r.data)
-               }
-               catch (e){
+                    callBack(r.data)
+                } catch (e) {
 
-                   console.warn(e)
-                   alert("Ошибка HTTP ");
-               }
-           }
-           input.style.display="none"
-           input.click();
-           document.body.appendChild(input)
+                    console.warn(e)
+                    alert("Ошибка HTTP ");
+                }
+            }
+            input.style.display = "none"
+            input.click();
+            document.body.appendChild(input)
         }
     },
-    watch:{
-        section:async function(){
-            if(this.section==0){
-                this.general=(await axios.get("/admin/general")).data;
+    watch: {
+        section: async function () {
+            if (this.section == 0) {
+                this.general = (await axios.get("/admin/general")).data;
             }
-            if(this.section==1){
-                this.videos=(await axios.get("/admin/videos")).data;
+            if (this.section == 1) {
+                this.videos = (await axios.get("/admin/videos")).data;
             }
-            if(this.section==2){
-                for(sect of this.sections){
-                    this.services[sect.id]= (await axios.get("/admin/service/"+sect.id)).data;
+            if (this.section == 2) {
+                for (sect of this.sections) {
+                    this.services[sect.id] = (await axios.get("/admin/service/" + sect.id)).data;
                 }
                 this.$forceUpdate();
 
             }
-            if(this.section==3){
-                for(sect of this.commands){
-                    this.geografy[sect.id]= (await axios.get("/admin/service/"+sect.id)).data;
+            if (this.section == 3) {
+                for (sect of this.commands) {
+                    this.geografy[sect.id] = (await axios.get("/admin/service/" + sect.id)).data;
                 }
                 this.$forceUpdate();
             }
         }
     },
-    mounted:async function(){
+    mounted: async function () {
         console.log("worked!")
-        this.sections=(await axios.get("/sections")).data;
-        this.commands=(await axios.get("/commands")).data;
+        this.sections = (await axios.get("/sections")).data;
+        this.commands = (await axios.get("/commands")).data;
 
-        this.general=(await axios.get("/admin/general")).data;
+        this.general = (await axios.get("/admin/general")).data;
         console.log(this.general)
     }
 })
