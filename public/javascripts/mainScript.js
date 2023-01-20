42fcd5b9aa1191c01a00437ee7ec3668695a3e1b
radoneje <53825282+radoneje@users.noreply.github.com>
2023-01-20 13:22:37 +0300

    s

--- public/javascripts/mainScript.js
+++ public/javascripts/mainScript.js
@@ -3,6 +3,8 @@ const showVideo=async(_id)=>{
     if(response.ok){
         let elem =document.createElement("div")
         elem.classList.add("fullScreenWr");
+        elem.classList.add("flex");
+        elem.classList.add("center");
         elem.innerHTML=await response.text();
         document.body.style.overflow="hidden";
         document.body.appendChild(elem)
