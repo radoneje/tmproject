5895b276b578811c2e0f7cbba6c85745c06be2e2
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-01 00:45:36 +0300

    s

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -29,7 +29,7 @@ router.post('/login', async function(req, res, next) {
   if(!req.body.pass || req.body.pass.length<2  || req.body.pass.length>20)
     return res.render("login", {name:req.body.name, msg:"Пароль неверен", passFocus:true})
 
-  let user = await req.db.collection('general').findOne({name:req.body.name, pass:req.body.pass})
+  let user = await req.db.collection('logins').findOne({name:req.body.name, pass:req.body.pass, isDeleted:false, isActive:true})
   if(!user)
     return res.render("login", {name:req.body.name, msg:"Пароль неверен", passFocus:true})
   req.session.admin=user;
