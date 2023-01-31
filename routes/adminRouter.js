90036b37ac54e9b3385953f775bdbb452fa03520
radoneje <53825282+radoneje@users.noreply.github.com>
2023-02-01 00:37:40 +0300

    s

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -9,13 +9,15 @@ const fs=require("fs")
 
 
 const adminAuth=(req, res, next)=>{
-
   if(!req.session.admin)
-    return res.redirect("/admin/login?redirect="+encodeURI(req.baseUrl))
+    return res.sendStatus(401)
+
   next();
 }
 /* GET users listing. */
-router.get('/', adminAuth, function(req, res, next) {
+router.get('/', function(req, res, next) {
+  if(!req.session.admin)
+    return res.redirect("/admin/login?redirect="+encodeURI(req.baseUrl))
   res.render("admin", {moment})
 });
 router.get('/login', function(req, res, next) {
